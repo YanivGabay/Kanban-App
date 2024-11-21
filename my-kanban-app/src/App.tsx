4f1ac8b36b5850958d91db7f12cc60ea6f2d04f9
yaniv <yaniv242@gmail.com>
2024-11-21 14:06:51 +0200

    created the 5 necc pages, added them to the routes without protection for now

--- my-kanban-app/src/App.tsx
+++ my-kanban-app/src/App.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
+import { Settings } from './pages/Settings'
+import { Profile } from './pages/Profile'
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+
 import './App.css'
 
 function App() {
@@ -9,8 +14,16 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/*home page - represent the dashboard */}
         <Route path="/" element={<Home />} />
-        
+        <Route path="/Settings" element= {<Settings />} />
+        {/*profile page */}
+        <Route path="/Profile" element= {<Profile />} />
+        {/*login page */}
+        <Route path="/Login" element= {<Login />} />
+        {/*register page */}
+        <Route path="/Register" element= {<Register />} />
+       
       </Routes>
 
     </Router>
