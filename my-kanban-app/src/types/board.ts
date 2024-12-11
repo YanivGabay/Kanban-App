3a4fa34e685b71ac33c949fcf4a6eed62a2ba155
yaniv <yaniv242@gmail.com>
2024-12-11 08:52:10 +0200

    adding new types

--- my-kanban-app/src/types/board.ts
+++ my-kanban-app/src/types/board.ts
@@ -1,8 +1,24 @@
 // src/types/types.ts
+
+
+export interface ListData {
+    id: number;
+    name: string;
+    cards: CardData[];
+  }
+  
+  export interface CardData {
+    id: number;
+    title: string;
+    description: string;
+    // we might add more fields here
+  }
+
 export interface BoardData {
     id: number;
     name: string;
     description: string;
+    lists: ListData[];
   }
   
  export interface BoardContextType {
@@ -10,5 +26,8 @@ export interface BoardData {
     addBoard: (name: string, description: string) => void;
     editBoard: (id: number, updatedBoard: Partial<BoardData>) => void;
     deleteBoard: (id: number) => void;
+    addList: (boardId: number, listName: string) => void;
+    addCard: (boardId: number, listId: number, cardTitle: string, cardDescription: string) => void;
+    //need to add: editList, deleteList, editCard, deleteCard
   }
   
